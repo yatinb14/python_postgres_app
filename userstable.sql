7b76fac9db52dcbfe9f051f5f2f7724bced1b31c
yatinb14 <65548009+yatinb14@users.noreply.github.com>
2021-06-23 15:10:21 +0530

    Create userstable.sql

---
+++ userstable.sql
@@ -0,0 +1,8 @@
+Create table if not exists userstables (
+	id serial PRIMARY KEY,
+	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+  town VARCHAR ( 50 ) NOT NULL,
+  password VARCHAR ( 50 ) NOT NULL,
+);
+
