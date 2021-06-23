bc6c56fc1415e48ca660dff841287d7805858c40
yatinb14 <65548009+yatinb14@users.noreply.github.com>
2021-06-23 15:10:56 +0530

    Update userstable.sql

--- userstable.sql
+++ userstable.sql
@@ -2,7 +2,7 @@ Create table if not exists userstables (
 	id serial PRIMARY KEY,
 	name VARCHAR ( 50 ) UNIQUE NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-  town VARCHAR ( 50 ) NOT NULL,
-  password VARCHAR ( 50 ) NOT NULL,
+  	town VARCHAR ( 50 ) NOT NULL,
+  	password VARCHAR ( 50 ) NOT NULL,
 );
 
